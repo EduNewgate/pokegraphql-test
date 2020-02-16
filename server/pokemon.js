25b7c3d14cf998f474e4cc5a6d05f9d60f5f2b52
EduNewgate <eduardo.moreno.93@gmail.com>
2020-02-16 23:21:50 +0100

    Changes

--- server/pokemon.js
+++ server/pokemon.js
@@ -17,14 +17,14 @@ class Pokemon extends RESTDataSource {
         });
         if (response != undefined) {
             for (const pokemon of response.results) {
-                await this.getPokemonByName(pokemon.name, pokemonCardResponse);
+                await this.getPokemonByName(pokemon.url, pokemonCardResponse);
             }
             return pokemonCardResponse;
         }
     }
 
-    async getPokemonByName(name, pokemonCardResponse) {
-        const response = await this.get(Constants.API_RESOURCE_POKEMON + '/' + name);
+    async getPokemonByName(url, pokemonCardResponse) {
+        const response = await this.get(url);
         var isResponse = response != undefined;
         if (isResponse && pokemonCardResponse != undefined) {
             this.buildCard(response, pokemonCardResponse);
@@ -34,10 +34,21 @@ class Pokemon extends RESTDataSource {
     }
 
     buildCard(data, pokemonCardResponse) {
+        this.formatNames(data.abilities);
         pokemonCardResponse.push({
-            "name": data.name,
-            "types": data.types.reverse()
+            "name": data.name.charAt(0).toUpperCase() + data.name.slice(1),
+            "weight": data.weight/10, //Kg
+            "height": data.height/10, //metros
+            "types": data.types.reverse(),
+            "abilities": data.abilities.reverse(),
+            "stats": data.stats.reverse()
         });
+    }
+
+    formatNames(abilities) {
+        for (let ab of abilities) {
+            ab.ability.name = ab.ability.name.charAt(0).toUpperCase() + ab.ability.name.replace("-", " ").slice(1)
+        }
     }
 }
 
--- server/schemas/custom/pokemon-card.js
+++ server/schemas/custom/pokemon-card.js
@@ -3,7 +3,11 @@ const { gql } = require('apollo-server');
 const PokemonCardModule = gql`
     type PokemonCard {
         name: String
+        weight: Float
+        height: Float
         types: [PokemonType]
+        abilities: [PokemonAbility]
+        stats: [PokemonStat]
     }
 `;
 
